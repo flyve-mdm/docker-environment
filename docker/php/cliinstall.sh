a1ca24b4da8c0e162d368cb8845cc28c5bcf5c25
Luis F. Gonzalez <ing.luisgonzalez23@gmail.com>
2018-06-05 18:56:14 -0400

    build(docker): configure all containers
    Signed-off-by: Luis F. Gonzalez <ing.luisgonzalez23@gmail.com>

---
+++ docker/php/cliinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+#
+# Instalation / Configuration GLPI
+#
+echo "Tests";
+echo $HOMEPATH;
+
+#if [ -e scripts/cliinstall.php ] ; then php scripts/cliinstall.php --db=glpitest --user=root --tests ; fi
+#if [ -e tools/cliinstall.php ] ; then php tools/cliinstall.php --db=glpitest --user=root --tests ; fi
