ff46b237e9d6b27fafb9559ae480312ee7c5c5e4
Luis F. Gonzalez <ing.luisgonzalez23@gmail.com>
2018-06-06 17:47:03 -0400

    build(docker): configure clone repo GLPI
    Signed-off-by: Luis F. Gonzalez <ing.luisgonzalez23@gmail.com>

--- docker/php/cliinstall.sh
+++ docker/php/cliinstall.sh
@@ -3,8 +3,14 @@ set -x
 #
 # Instalation / Configuration GLPI
 #
-echo "Tests";
-echo $HOMEPATH;
-
+echo "Cloning GLPI from source:" $GLPI_SOURCE;
+# git clone --depth=1 $GLPI_SOURCE -b $GLPI_TAG $HOMEPATH
+# composer install --no-dev --no-interaction
+cd $HOMEPATH
+#TODO: Pending for validing this part / Testing the viability of this
+# curl -O -L "https://github.com/glpi-project/glpi/releases/download/$GLPI_TAG/glpi-$GLPI_TAG.tgz" && \
+#     tar -xzf "glpi-$GLPI_TAG.tgz" --strip 1 && \
+#     rm "glpi-$GLPI_TAG.tgz"
+git clone --depth=1 $GLPI_SOURCE -b $GLPI_TAG $HOMEPATH
 #if [ -e scripts/cliinstall.php ] ; then php scripts/cliinstall.php --db=glpitest --user=root --tests ; fi
 #if [ -e tools/cliinstall.php ] ; then php tools/cliinstall.php --db=glpitest --user=root --tests ; fi
