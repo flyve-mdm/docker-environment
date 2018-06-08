4bf869cc5e05273f8d67d8b0c38a565520e28fc0
Luis F. Gonzalez <lgonzalez@teclib.com>
2018-06-08 18:34:11 -0400

    build(docker): change the form to clone
    add validation if www is empty
    
    Signed-off-by: Luis F. Gonzalez <lgonzalez@teclib.com>

--- docker/php/cliinstall.sh
+++ docker/php/cliinstall.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-set -x
+set -e
 #
 # Instalation / Configuration GLPI
 #
-echo "Cloning GLPI from source:" $GLPI_SOURCE;
-# git clone --depth=1 $GLPI_SOURCE -b $GLPI_TAG $HOMEPATH
-# composer install --no-dev --no-interaction
-cd $HOMEPATH
-#TODO: Pending for validing this part / Testing the viability of this
-# curl -O -L "https://github.com/glpi-project/glpi/releases/download/$GLPI_TAG/glpi-$GLPI_TAG.tgz" && \
-#     tar -xzf "glpi-$GLPI_TAG.tgz" --strip 1 && \
-#     rm "glpi-$GLPI_TAG.tgz"
-git clone --depth=1 $GLPI_SOURCE -b $GLPI_TAG $HOMEPATH
-#if [ -e scripts/cliinstall.php ] ; then php scripts/cliinstall.php --db=glpitest --user=root --tests ; fi
-#if [ -e tools/cliinstall.php ] ; then php tools/cliinstall.php --db=glpitest --user=root --tests ; fi
+if [ "$(ls -A $HOMEPATH)" ]; then 
+    echo "The directory of site don't is empty" 
+    echo "If you want cloning GLPI, remove all content in directory www and try again." 
+else
+    echo "Cloning GLPI from source:" $GLPI_SOURCE;
+    #TODO: Pending for validing this part / Testing the viability of this
+    git clone --depth=1 $GLPI_SOURCE -b $GLPI_BRANCH $HOMEPATH
+    cd $HOMEPATH
+    composer install --no-dev --no-interaction
+    #TODO: Pending for test unit
+    #if [ -e scripts/cliinstall.php ] ; then php scripts/cliinstall.php --db=glpitest --user=root --tests ; fi
+    #if [ -e tools/cliinstall.php ] ; then php tools/cliinstall.php --db=glpitest --user=root --tests ; fi
+fi
+
