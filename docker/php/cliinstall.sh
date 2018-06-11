4d860873ee40ce70c8cbcdb356b672cce20c6f3f
Luis F. Gonzalez <lgonzalez@teclib.com>
2018-06-11 17:24:26 -0400

    build(docker): change cliinstall
    add command in compose
    change directory of www-data
    add permission in directories after cloning
    composer install as www-data user
    
    Signed-off-by: Luis F. Gonzalez <lgonzalez@teclib.com>

--- docker/php/cliinstall.sh
+++ docker/php/cliinstall.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-set -e
 #
 # Instalation / Configuration GLPI
 #
-if [ "$(ls -A $HOMEPATH)" ]; then 
-    echo "The directory of site don't is empty" 
-    echo "If you want cloning GLPI, remove all content in directory www and try again." 
+if [ "$(ls -A $HOMEPATH)" ]; then
+    echo "The directory of site don't is empty"
+    echo "If you want cloning GLPI, remove all content in directory www and try again."
+    su - www-data -c 'composer install --no-dev --no-interaction'
 else
     echo "Cloning GLPI from source:" $GLPI_SOURCE;
+    echo "Checkout in the Branch:" $GLPI_BRANCH;
     #TODO: Pending for validing this part / Testing the viability of this
-    git clone --depth=1 $GLPI_SOURCE -b $GLPI_BRANCH $HOMEPATH
-    cd $HOMEPATH
-    composer install --no-dev --no-interaction
+    git clone --progress --depth=1 $GLPI_SOURCE -b $GLPI_BRANCH $HOMEPATH
+    # Add permission to the folder
+    chown -R www-data:www-data $HOMEPATH /var/www
+    echo "Installing packages";
+    su - www-data -c 'composer install --no-dev --no-interaction'
     #TODO: Pending for test unit
     #if [ -e scripts/cliinstall.php ] ; then php scripts/cliinstall.php --db=glpitest --user=root --tests ; fi
     #if [ -e tools/cliinstall.php ] ; then php tools/cliinstall.php --db=glpitest --user=root --tests ; fi
 fi
-
+php-fpm -F
