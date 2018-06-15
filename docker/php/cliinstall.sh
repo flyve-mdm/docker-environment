60d5b3f9e71c23baf8061492052d81fc6b553585
Luis F. Gonzalez <lgonzalez@teclib.com>
2018-06-15 13:55:53 -0400

    build(docker): add flyvemdm and fusioninventory
    add variables for flyvemdm cloning
    add variables for fusioninventory cloning
    
    Signed-off-by: Luis F. Gonzalez <lgonzalez@teclib.com>

--- docker/php/cliinstall.sh
+++ docker/php/cliinstall.sh
@@ -9,14 +9,31 @@ if [ "$(ls -A $HOMEPATH)" ]; then
 else
     echo "Cloning GLPI from source:" $GLPI_SOURCE;
     echo "Checkout in the Branch:" $GLPI_BRANCH;
-    #TODO: Pending for validing this part / Testing the viability of this
     git clone --progress --depth=1 $GLPI_SOURCE -b $GLPI_BRANCH $HOMEPATH
-    # Add permission to the folder
-    chown -R www-data:www-data $HOMEPATH /var/www
     echo "Installing packages...";
     su - www-data -c 'composer install --no-dev --no-interaction'
 fi
 #Configuring cron for GLPI
 echo "Configuring cron for GLPI..."
 echo "*/2 * * * * www-data /usr/local/bin/php $HOMEPATH/front/cron.php &>/dev/null" >> /etc/cron.d/glpi
+# Instalation / Configuration Flyvemdm and Fusioninventory
+#
+if [ -d $HOMEPATH$GLPI_PLUGINS_DIRECTORY$FLYVEMDM_PATH ] && [ "$(ls -A $HOMEPATH$GLPI_PLUGINS_DIRECTORY$FLYVEMDM_PATH)" ]; then
+    echo "The directory's Flyvemdm exist"
+    echo "If you want reinstall, remove the directory $FLYVEMDM_PATH and $FUSIONINVENTORY_PATH, and try again."
+    cd $HOMEPATH$GLPI_PLUGINS_DIRECTORY$FLYVEMDM_PATH
+    su - www-data -c 'composer install --no-dev'
+else
+    echo "Cloning Fusioninventory from source:" $FUSIONINVENTORY_SOURCE;
+    echo "Checkout in the Branch:" $FUSIONINVENTORY_BRANCH;
+    git clone --progress --depth=1 $FUSIONINVENTORY_SOURCE -b $FUSIONINVENTORY_BRANCH $HOMEPATH$GLPI_PLUGINS_DIRECTORY$FUSIONINVENTORY_PATH
+    echo "Cloning Flyvemdm from source:" $FLYVEMDM_SOURCE;
+    echo "Checkout in the Branch:" $FLYVEMDM_BRANCH;
+    git clone --progress --depth=1 $FLYVEMDM_SOURCE -b $FLYVEMDM_BRANCH $HOMEPATH$GLPI_PLUGINS_DIRECTORY$FLYVEMDM_PATH
+    cd $HOMEPATH$GLPI_PLUGINS_DIRECTORY$FLYVEMDM_PATH
+    echo "Installing packages...";
+    su - www-data -c 'composer install --no-dev'
+fi
+# Add permission to the folder
+chown -R www-data:www-data $HOMEPATH /var/www
 php-fpm -F
