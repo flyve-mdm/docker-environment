7e2c827e0329eb7c0d5ac4ff11053fb32dce3382
Luis F. Gonzalez <lgonzalez@teclib.com>
2018-07-03 11:47:52 -0400

    build(docker): create command flyvemdm-test
    create command executable in every environment
    
    Signed-off-by: Luis F. Gonzalez <lgonzalez@teclib.com>

--- docker/php/testone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "=== Unit tests ==="
-if [ -f $1 ]; then
-    RESOURCE_TYPE="-f"
-elif [ -d $1 ]; then
-    RESOURCE_TYPE="-d"
-fi
-$PHP $HOMEPATH/plugins/flyvemdm/vendor/bin/atoum --debug -bf $HOMEPATH/plugins/flyvemdm/tests/bootstrap.php -no-cc $RESOURCE_TYPE $1
