2fc2cbb33161fec5afa843d598746c60eba3da01
Domingo Oropeza <doropeza@teclib.com>
2018-09-07 18:18:15 -0400

    refactor: build script to install all by default
    Signed-off-by: Domingo Oropeza <doropeza@teclib.com>

--- docker/php/cliinstall.sh
+++ docker/php/cliinstall.sh
@@ -2,37 +2,59 @@
 #
 # Instalation / Configuration GLPI
 #
+
+su - www-data
+
 if [ "$(ls -A $GLPIPATH)" ]; then
     echo "The directory of site don't is empty"
     echo "If you want cloning GLPI, remove all content in directory www and try again."
-    su - www-data -c 'composer install --no-dev --no-interaction'
 else
     echo "Cloning GLPI from source:" $GLPI_SOURCE;
     echo "Checkout in the Branch:" $GLPI_BRANCH;
-    git clone --progress --depth=1 $GLPI_SOURCE -b $GLPI_BRANCH $GLPIPATH
-    echo "Installing packages...";
-    su - www-data -c 'composer install --no-dev --no-interaction'
+    git clone --quiet -v --depth 1 $GLPI_SOURCE -b $GLPI_BRANCH $GLPIPATH
+    cd $GLPIPATH
+    echo "Installing GLPI packages...";
+    composer install --no-dev --no-interaction
+    PHPSCRIPT='scripts/cliinstall.php';
+    if [ -e tools/cliinstall.php ] ; then
+       PHPSCRIPT='tools/cliinstall.php';
+    fi
+    php $PHPSCRIPT --host=dockerdb --user=root --pass=$MYSQL_ROOT_PASSWORD --db=$GLPI_DB_NAME --force;
+
+    #Configuring cron for GLPI
+    echo "Configuring cron for GLPI..."
+    echo "*/2 * * * * www-data /usr/local/bin/php $GLPIPATH/front/cron.php &>/dev/null" >> /etc/cron.d/glpi
 fi
-#Configuring cron for GLPI
-echo "Configuring cron for GLPI..."
-echo "*/2 * * * * www-data /usr/local/bin/php $GLPIPATH/front/cron.php &>/dev/null" >> /etc/cron.d/glpi
-# Instalation / Configuration Flyvemdm and Fusioninventory
-#
-if [ -d $GLPIPATH/plugins$FLYVEMDM_PATH ] && [ "$(ls -A $GLPIPATH/plugins$FLYVEMDM_PATH)" ]; then
+
+cd $GLPIPATH
+
+# Instalation / Configuration of Fusioninventory
+if [ -d plugins$FUSIONINVENTORY_PATH ] && [ "$(ls -A plugins$FUSIONINVENTORY_PATH)" ]; then
     echo "The directory's Flyvemdm exist"
-    echo "If you want reinstall, remove the directory $FLYVEMDM_PATH and $FUSIONINVENTORY_PATH, and try again."
+    echo "If you want reinstall, remove the directory and try again."
 else
     echo "Cloning Fusioninventory from source:" $FUSIONINVENTORY_SOURCE;
     echo "Checkout in the Branch:" $FUSIONINVENTORY_BRANCH;
-    git clone --progress --depth=1 $FUSIONINVENTORY_SOURCE -b $FUSIONINVENTORY_BRANCH $GLPIPATH/plugins$FUSIONINVENTORY_PATH
+    git clone --quiet -v --depth 1 $FUSIONINVENTORY_SOURCE -b $FUSIONINVENTORY_BRANCH plugins$FUSIONINVENTORY_PATH
+    php plugins$FUSIONINVENTORY_PATH/scripts/cli_install.php --as-user glpi
+fi
+
+# Instalation / Configuration Flyvemdm
+if [ -d plugins$FLYVEMDM_PATH ] && [ "$(ls -A plugins$FLYVEMDM_PATH)" ]; then
+    echo "The directory's Flyvemdm exist"
+    echo "If you want reinstall, remove the directory and try again."
+else
     echo "Cloning Flyvemdm from source:" $FLYVEMDM_SOURCE;
     echo "Checkout in the Branch:" $FLYVEMDM_BRANCH;
-    git clone --progress $FLYVEMDM_SOURCE -b $FLYVEMDM_BRANCH $GLPIPATH/plugins$FLYVEMDM_PATH
+    git clone --quiet $FLYVEMDM_SOURCE -b $FLYVEMDM_BRANCH plugins$FLYVEMDM_PATH
+    echo "Installing packages...";
+    cd plugins$FLYVEMDM_PATH
+    composer install
+    php tools/cli_install.php --enable-api --mqtt-internal-address dockerMosquitto \
+        --mqtt-port $HOST_SERVER_MOSQUITTO_PORT --mqtt-port-tls $HOST_SERVER_MOSQUITTO_PORT_TLS
 fi
-echo "Installing packages...";
-su - www-data -c "cd $GLPIPATH/plugins$FLYVEMDM_PATH && composer install"
-su - www-data -c "$PHP $GLPIPATH/plugins$FLYVEMDM_PATH/tools/cli_install.php --enable-api --mqtt-internal-address $MOSQUITTO_BROKER_INTERNAL_ADDRESS --mqtt-port $HOST_SERVER_MOSQUITTO_PORT --mqtt-port-tls $HOST_SERVER_MOSQUITTO_PORT_TLS"
 
 # Add permission to the folder
-chown -R www-data:www-data $GLPIPATH /var/www
+chmod -R 777 $GLPIPATH/files
+chown -R www-data:www-data $GLPIPATH
 php-fpm -F
